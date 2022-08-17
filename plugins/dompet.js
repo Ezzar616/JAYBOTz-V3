e4a8803694872cd7e6b69f6896021cb6aaa02db2
JAYBOTz <106862955+EkaBotWhatsapp@users.noreply.github.com>
2022-08-17 16:03:49 +0000

    Add files via upload

--- plugins/dompet.js
+++ plugins/dompet.js
@@ -3,9 +3,9 @@ let handler = async (m) => {
     if (m.isGroup) who = m.mentionedJid[0] ? m.mentionedJid[0] : m.sender
     else who = m.sender
     if (typeof db.data.users[who] == 'undefined') throw 'Pengguna tidak ada didalam data base'
-    m.reply(`${global.db.data.users[who].money} Your money`)
+    m.reply(`${global.db.data.users[who].level} Your Level\n${global.db.data.users[who].money} Your Money\n${global.db.data.users[who].bank} Your Bank\n${global.db.data.users[who].limit} Your Limit\n${global.db.data.users[who].exp} Your Exp`)
 }
 handler.help = ['dompet [@user]']
 handler.tags = ['xp']
-handler.command = /^(dompet)$/i
+handler.command = /^(dompet|duit)$/i
 module.exports = handler
